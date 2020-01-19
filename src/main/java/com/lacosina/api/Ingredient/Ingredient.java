3be445b4a278b2e69e6c041bad58baab831bfe35
IfYouSeeKei <khiragi0716@gmail.com>
2020-01-19 22:34:01 +0900

    Ingredient CRUD

---
+++ src/main/java/com/lacosina/api/Ingredient/Ingredient.java
@@ -0,0 +1,36 @@
+package com.lacosina.api.Ingredient;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name="ingredient_name")
+    private String ingredientName;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="category")
+    private String category;
+
+    public Ingredient(String name, String description, String category) {
+        //validate dn dto bago iset?
+        this.setIngredientName(name);
+        this.setDescription(description);
+        this.setCategory(category);
+    }
+
+    public Ingredient(String name, String description) {
+        //validate dn dto bago iset?
+        this.setIngredientName(name);
+        this.setDescription(description);
+    }
+}
---
+++ src/main/java/com/lacosina/api/Ingredient/IngredientService.java
@@ -0,0 +1,42 @@
+package com.lacosina.api.Ingredient;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class IngredientService {
+
+    private final IngredientRepository ingredientRepository;
+
+    public IngredientService(IngredientRepository ingredientRepository){
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @Transactional
+    public Ingredient createIngredient(final String name, final String description, final String category){
+        final Ingredient ingredient = new Ingredient(name,description,category);
+        return this.ingredientRepository.save(ingredient);
+    }
+
+    @Transactional
+    public Ingredient updateIngredient(final int id, Ingredient ingredient){
+        Ingredient ingredientUpdate = this.ingredientRepository.findById(id).orElseThrow();
+        ingredientUpdate = ingredient;
+        return this.ingredientRepository.save(ingredientUpdate);
+    }
+
+    @Transactional
+    public Ingredient getIngredient(final int id){
+        return this.ingredientRepository.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    public Boolean deleteIngredient(final int id) {
+        try {
+            this.ingredientRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
