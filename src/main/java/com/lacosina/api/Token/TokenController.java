581a05ceed7b7e50e7f031ce608efefce12e6997
scottypipz <scottypipz@gmail.com>
2020-02-26 23:19:12 +0800

    Made progress oauth

--- src/main/java/com/lacosina/api/Token/TokenController.java
+++ src/main/java/com/lacosina/api/Token/TokenController.java
@@ -1,14 +1,19 @@
 package com.lacosina.api.Token;
 
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 public class TokenController {
@@ -24,4 +29,26 @@ public class TokenController {
     public void revokeToken(HttpServletRequest request, @PathVariable String tokenId) {
         tokenServices.revokeToken(tokenId);
     }
+
+    @GetMapping("/tokens")
+    @ResponseBody
+    public List<String> getTokens() {
+        Collection<OAuth2AccessToken> tokens = tokenStore.findTokensByClientId("sampleClientId");
+        return Optional.ofNullable(tokens)
+                    .orElse(Collections.emptyList())
+                    .stream()
+                    .map(OAuth2AccessToken::getValue)
+                    .collect(Collectors.toList());
+    }
+
+    @PostMapping("/tokens/revokeRefreshToken/{tokenId:.*}")
+    @ResponseBody
+    public String revokeAccessToken(@PathVariable String tokenId) {
+        if (tokenStore instanceof JdbcTokenStore) {
+            ((JdbcTokenStore) tokenStore).removeRefreshToken(tokenId);
+        }
+
+        return tokenId;
+    }
+
 }
