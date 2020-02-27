09c5ece0c14c40aef1843f79fcdaef005b6c2b9a
scottypipz <scottypipz@gmail.com>
2020-02-27 23:14:39 +0800

    Made progress oauth

--- src/main/java/com/lacosina/api/LacosinaApplication.java
+++ src/main/java/com/lacosina/api/LacosinaApplication.java
@@ -2,9 +2,10 @@ package com.lacosina.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LacosinaApplication {
+public class LacosinaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LacosinaApplication.class, args);
